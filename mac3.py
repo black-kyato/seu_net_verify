5120d29f1e54ab4ac678b79cd5021ba0eb55c459
black-kyato <36191481+black-kyato@users.noreply.github.com>
2020-09-16 21:35:03 +0800

    Update mac3.py
    增加异常处理

--- mac3.py
+++ mac3.py
@@ -6,8 +6,11 @@ import os
 
 # get ip
 def get_host_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('8.8.8.8', 80))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+    except BaseException:
+        pass
     ip = s.getsockname()[0]
     s.close()
     return ip
